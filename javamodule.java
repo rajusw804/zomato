674bfa64c9591461d1ec89d1c88644df7febb3b1
raj <rajusw804@example.com>
2025-02-25 08:04:20 +0530

    adding java file

---
+++ javamodule.java
@@ -0,0 +1,6 @@
+public class Exercise1 { 
+    public static void main(String[] args) {
+        // Print a greeting message to the console
+        System.out.println("Hello\nAlexandra Abramov!");
+    } 
+}
